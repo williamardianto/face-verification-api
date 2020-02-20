31b67ac2059736dc828059906b82c374ef2f7b04
William Ardianto <william.ardianto@gmail.com>
2020-02-20 14:42:01 +0800

    update controller
    Former-commit-id: 96f216d9c9c3776532066d2a9192ad26183a08f0

--- src/main/java/com/example/faceverification/Controller.java
+++ src/main/java/com/example/faceverification/Controller.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,18 +30,18 @@ public class Controller {
     public ResponseEntity verify(
             @RequestParam(required = false) MultipartFile image1_file,
             @RequestParam(required = false) MultipartFile image2_file,
-            @RequestParam(required = false) String image1_base64,
-            @RequestParam(required = false) String image2_base64
+            @RequestBody(required = false) Map<String, String> image_base64
     ) throws IOException {
 
         File f1, f2;
 
-        if (image1_file != null && image2_file != null){
+        if (image1_file != null && image2_file != null) {
             f1 = Utils.convert(image1_file);
             f2 = Utils.convert(image2_file);
-        }else if(!Utils.isNullOrEmpty(image1_base64) && !Utils.isNullOrEmpty(image2_base64)){
-            f1 = Utils.base64ToFile(image1_base64);
-            f2 = Utils.base64ToFile(image2_base64);
+        }else if(image_base64 != null && image_base64.get("image1_base64") != null &&
+                image_base64.get("image2_base64") != null){
+            f1 = Utils.base64ToFile(image_base64.get("image1_base64"));
+            f2 = Utils.base64ToFile(image_base64.get("image2_base64"));
         }else{
             Map<String, String> body = new HashMap<>();
             body.put("message", "incorrect input data");
