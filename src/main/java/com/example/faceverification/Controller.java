439df290d089612463167736f59ac726388eaf48
William Ardianto <william.ardianto@gmail.com>
2020-02-24 12:25:51 +0800

    update pom

--- src/main/java/com/example/faceverification/Controller.java
+++ src/main/java/com/example/faceverification/Controller.java
@@ -1,23 +1,15 @@
 package com.example.faceverification;
 
-import org.deeplearning4j.nn.graph.ComputationGraph;
-import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
-import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
-import org.deeplearning4j.util.ModelSerializer;
-import org.nd4j.linalg.api.ndarray.INDArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,18 +22,17 @@ public class Controller {
     public ResponseEntity verify(
             @RequestParam(required = false) MultipartFile image1_file,
             @RequestParam(required = false) MultipartFile image2_file,
-            @RequestBody(required = false) Map<String, String> image_base64
+            @RequestParam(required = false) String image1_base64,
+            @RequestParam(required = false) String image2_base64
     ) throws IOException {
-
         File f1, f2;
 
         if (image1_file != null && image2_file != null) {
             f1 = Utils.convert(image1_file);
             f2 = Utils.convert(image2_file);
-        }else if(image_base64 != null && image_base64.get("image1_base64") != null &&
-                image_base64.get("image2_base64") != null){
-            f1 = Utils.base64ToFile(image_base64.get("image1_base64"));
-            f2 = Utils.base64ToFile(image_base64.get("image2_base64"));
+        }else if(!Utils.isNullOrEmpty(image1_base64) && !Utils.isNullOrEmpty(image2_base64)){
+            f1 = Utils.base64ToFile(image1_base64);
+            f2 = Utils.base64ToFile(image2_base64);
         }else{
             Map<String, String> body = new HashMap<>();
             body.put("message", "incorrect input data");
